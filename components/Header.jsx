df844869e38c222a3f56e5b1d72fa4059e5c4e2d
QuicklyDG <quicklydeguzman@gmail.com>
2026-07-31 00:45:03 +0800

    first commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,5 @@
+import { FaEnvelope, FaPhone, FaSearch } from "react-icons/fa";
+
 export default function Header() {
   return (
     <header className="header-one header--sticky">
@@ -7,13 +9,29 @@ export default function Header() {
             <div className="header-top">
               <div className="left">
                 <div className="map-area">
-                  <i className="far fa-envelope" aria-hidden="true" style={{ fontSize: '12px', marginRight: '6px', lineHeight: 1 }} />
-                  <a href="mailto:info@empireonehealth.com">info@empireonehealth.com</a>
+                  <FaEnvelope
+                    style={{
+                      fontSize: "12px",
+                      marginRight: "6px",
+                      lineHeight: 1,
+                      color: "#ffffff",
+                    }}
+                  />
+                  <a href="mailto:info@empireonehealth.com">
+                    info@empireonehealth.com
+                  </a>
                 </div>
               </div>
               <div className="right">
                 <div className="map-area">
-                  <i className="far fa-phone fa-flip-horizontal" aria-hidden="true" style={{ fontSize: '12px', marginRight: '6px', lineHeight: 1 }} />
+                  <FaPhone
+                    style={{
+                      fontSize: "12px",
+                      marginRight: "6px",
+                      lineHeight: 1,
+                      color: "#ffffff",
+                    }}
+                  />
                   <a href="tel:+18332006002">+1 (833) 200-6002</a>
                 </div>
               </div>
@@ -27,7 +45,13 @@ export default function Header() {
             <div className="header-wrapper-1">
               <div className="logo-area-start">
                 <a href="/" className="logo">
-                  <img src="/assets/images/logo/empireone-health-logo.png" alt="EmpireOne Health" />
+                  <img
+                    src="/assets/images/logo/empireone-health-logo.png"
+                    alt="EmpireOne Health"
+                    width="220"
+                    height="60"
+                    fetchPriority="high"
+                  />
                 </a>
                 <div className="nav-area">
                   <ul>
@@ -58,16 +82,29 @@ export default function Header() {
                 </div>
               </div>
               <div className="header-right">
-                <div className="input-area">
+                {/* <div className="input-area">
                   <input id="myInput" type="text" placeholder="Search..." />
                   <i className="fa-light fa-magnifying-glass" />
-                </div>
-                <a href="#" className="rts-btn btn-primary book-call-popup-trigger" data-popup-form-trigger="book-call">
+                </div> */}
+                <a
+                  href="#"
+                  className="rts-btn btn-primary book-call-popup-trigger"
+                  data-popup-form-trigger="book-call"
+                >
                   Get in touch
-                  <img src="/assets/images/banner/icons/arrow--up-right.svg" alt="" />
+                  <img
+                    src="/assets/images/banner/icons/arrow--up-right.svg"
+                    alt=""
+                  />
                 </a>
                 <div className="menu-btn" id="menu-btn">
-                  <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg
+                    width="20"
+                    height="16"
+                    viewBox="0 0 20 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <rect y="14" width="20" height="2" fill="#1F1F25" />
                     <rect y="7" width="20" height="2" fill="#1F1F25" />
                     <rect width="20" height="2" fill="#1F1F25" />
